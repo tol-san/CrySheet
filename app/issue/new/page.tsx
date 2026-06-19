91de83a53a8d1a3d5e8a0952a6e93306661dff42
San tol <tolsan42@gmail.com>
2026-06-20 01:34:52 +0700

    Handle form submit for creating issue

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -3,13 +3,39 @@ import {Button, TextField} from "@radix-ui/themes"
 import MDEditor from '@uiw/react-md-editor';
 
 import React from 'react'
+import {Controller, useForm} from "react-hook-form";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
+
 
 export default function NewIssuePage() {
+    const router = useRouter();
+    const {register, control, handleSubmit} = useForm<IssueForm>()
+
     return (
-        <div className="max-w-xl space-y-4">
-            <TextField.Root size="3" placeholder="Title" />
-            <MDEditor/>
+        <form
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post("/api/issue",data);
+                router.push("/issue")
+            })}
+            className="max-w-xl space-y-4">
+            <TextField.Root size="3" placeholder="Title" {...register("title")} />
+            <Controller
+                name="description"
+                control={control}
+                defaultValue=""
+                render={({field}) => (
+                    <MDEditor
+                        onChange={val => field.onChange(val ?? "")}
+                        value={field.value}/>
+                )}/>
+
             <Button>Submit</Button>
-        </div>
+        </form>
     )
 }
