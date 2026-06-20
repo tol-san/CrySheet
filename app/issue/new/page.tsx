fa009f55ce0d6bb66cc6455c4c5efb1b510077ff
San tol <tolsan42@gmail.com>
2026-06-20 13:09:21 +0700

    Implement client-side validation

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,41 +1,71 @@
 "use client"
-import {Button, TextField} from "@radix-ui/themes"
+import {Button, Callout, Text, TextField} from "@radix-ui/themes"
 import MDEditor from '@uiw/react-md-editor';
 
-import React from 'react'
+import React, {useState} from 'react'
 import {Controller, useForm} from "react-hook-form";
 import {useRouter} from "next/navigation";
 import axios from "axios";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {createIssueSchema} from "@/app/validationSchema";
+import {z} from 'zod'
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 
 export default function NewIssuePage() {
     const router = useRouter();
-    const {register, control, handleSubmit} = useForm<IssueForm>()
+    const [error, setError] = useState<string>();
+    const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
 
     return (
-        <form
-            onSubmit={handleSubmit(async (data) => {
-                await axios.post("/api/issue",data);
-                router.push("/issue")
-            })}
-            className="max-w-xl space-y-4">
-            <TextField.Root size="3" placeholder="Title" {...register("title")} />
-            <Controller
-                name="description"
-                control={control}
-                defaultValue=""
-                render={({field}) => (
-                    <MDEditor
-                        onChange={val => field.onChange(val ?? "")}
-                        value={field.value}/>
-                )}/>
+        <div className="max-w-xl ">
+            {error && <Callout.Root color="red" className="mb-5">
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+            <form
+                className={"space-y-4"}
+                onSubmit={handleSubmit(async (data) => {
+                    try {
+                        await axios.post("/api/issue", data);
+                        router.push("/issue")
+                    } catch {
+                        setError("An unexpected error occurred.");
+                    }
+                })}>
+                <div>
+                    <TextField.Root
+                        size="3"
+                        placeholder="Title"
+                        {...register("title")}
+                    >
+                    </TextField.Root>
+                    {errors.title && (
+                        <Text color="red" as="p">
+                            {errors.title.message}
+                        </Text>
+                    )}
+                </div>
+                <div>
+                    <Controller
+                        name="description"
+                        control={control}
+                        defaultValue=""
+                        render={({field}) => (
+                            <MDEditor
+                                onChange={val => field.onChange(val ?? "")}
+                                value={field.value}/>
+                        )}/>
+                    {
+                        errors.description &&
+                        <Text className={"mb-5"} color={"red"} as={"p"}>{errors.description.message}</Text>
+                    }
+                </div>
 
-            <Button>Submit</Button>
-        </form>
+                <Button type={"submit"}>Submit</Button>
+            </form>
+        </div>
     )
 }
