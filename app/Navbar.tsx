8e46c50571e9a3ec94a7240b5fc42bfcc8636fa3
San tol <tolsan42@gmail.com>
2026-06-28 02:20:10 +0700

    Refactoring navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -8,72 +8,84 @@ import {useSession} from "next-auth/react";
 import {Box, Container, DropdownMenu, Flex, Text} from "@radix-ui/themes";
 
 export default function Navbar() {
+
+    return (
+        <nav className={" border-b items-center border-zinc-300 py-4 px-4"}>
+            <Container>
+                <Flex align={"center"} justify={"between"}>
+                    <NavLinks/>
+                    <AuthState/>
+                </Flex>
+            </Container>
+        </nav>
+    )
+}
+
+const NavLinks = () => {
     const currentPath = usePathname();
-    const {status, data: session} = useSession();
     const links = [
         {label: "Dashboard", href: "/"},
         {label: "Issue", href: "/issue"}
     ]
     return (
-        <nav className={" border-b items-center border-zinc-300 py-4 px-4"}>
-            <Container>
-                <Flex align={"center"} justify={"between"}>
-                    <Flex align={"center"}>
-                        <Link href="/">
-                            <Image
-                                src="/logo.png"
-                                height={50}
-                                width={50}
-                                className="h-auto w-auto mr-2"
-                                alt="Logo"/>
+        <Flex align={"center"}>
+            <Link href="/">
+                <Image
+                    src="/logo.png"
+                    height={50}
+                    width={50}
+                    className="h-auto w-auto mr-2"
+                    alt="Logo"/>
+            </Link>
+            <ul className="flex space-x-4">
+                {links.map(link => (
+                    <li key={link.href}>
+                        <Link
+                            href={link.href}
+                            className={classname({
+                                "text-zinc-900": link.href === currentPath,
+                                "text-zinc-500": link.href !== currentPath,
+                                "hover:text-zinc-800 transition-colors": true
+                            })}
+                        >
+                            {link.label}
                         </Link>
-                        <ul className="flex space-x-4">
-                            {links.map(link => (
-                                <li key={link.href}>
-                                    <Link
-                                        href={link.href}
-                                        className={classname({
-                                            "text-zinc-900": link.href === currentPath,
-                                            "text-zinc-500": link.href !== currentPath,
-                                            "hover:text-zinc-800 transition-colors": true
-                                        })}
-                                    >
-                                        {link.label}
-                                    </Link>
-                                </li>))}
-                        </ul>
-                    </Flex>
-                    <Box>
-                        {status === "authenticated" && (
-                            <DropdownMenu.Root>
-                                <DropdownMenu.Trigger>
-                                    <Image
-                                        src={session.user?.image ?? '/default-avatar.png'}
-                                        alt={session.user?.name ?? 'User avatar'}
-                                        width={45}
-                                        height={45}
-                                        className={"rounded-full cursor-pointer"}
-                                    />
+                    </li>))}
+            </ul>
+        </Flex>
+    )
+}
+const AuthState = () => {
+    const {status, data: session} = useSession();
+    if (status === "loading") return null;
+    if (status === "unauthenticated")
+        return <Link className={"text-zinc-500"}  href={"/api/auth/signin"}>Login</Link>
 
-                                </DropdownMenu.Trigger>
-                                <DropdownMenu.Content>
-                                    <DropdownMenu.Label>
-                                        <Text>{session?.user?.email}</Text>
-                                    </DropdownMenu.Label>
-                                    <DropdownMenu.Item>
-                                        <Text>
-                                            <Link href={"/api/auth/signout"}>Sign Out</Link>
-                                        </Text>
-                                    </DropdownMenu.Item>
-                                </DropdownMenu.Content>
-                            </DropdownMenu.Root>
 
-                        )}
+    return (
+        <Box>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Image
+                        src={session!.user?.image ?? '/default-avatar.png'}
+                        alt={session!.user?.name ?? 'User avatar'}
+                        width={45}
+                        height={45}
+                        className={"rounded-full cursor-pointer"}
+                    />
 
-                        {status === "unauthenticated" && <Link href={"/api/auth/signin"}>Login</Link>}
-                    </Box>
-                </Flex>
-            </Container>
-        </nav>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                        <Text>{session?.user?.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                        <Text >
+                            <Link href={"/api/auth/signout"}>Sign Out</Link>
+                        </Text>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </Box>
     )
 }
